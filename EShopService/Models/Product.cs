75205c0731feefc8c0d4b9e9afa97a2fcbe047ad
djkostyan4ik <kostyamalinovskiy16@gmail.com>
2025-03-04 15:38:06 +0100

    Add project files.

---
+++ EShopService/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace EShopService.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = default!;
+
+    public string Ean { get; set; } = default!;
+
+    public decimal Price { get; set; }
+
+    public string Sku { get; set; } = default;
+
+    public Category Category { get; set; }
+
+    public bool Deleted { get; set; } = false;
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public Guid CreatedBy { get; set; }
+
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+    public Guid UpdatedBy { get; set; }
+
+}
